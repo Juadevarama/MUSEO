17f4b2cf0bcfb55ed212732acff90e32d0048a06
GalacticKoala2 <juavarama@gmail.com>
2021-01-03 20:15:00 +0100

    Camibando los paquetes

--- src/main/java/TFG/TfgApplication.java
+++ src/main/java/TFG/TfgApplication.java
@@ -1,13 +1,13 @@
-package com.example.project;
+package TFG;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjectApplication {
+public class TfgApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProjectApplication.class, args);
+		SpringApplication.run(TfgApplication.class, args);
 	}
 
 }
--- src/test/java/TFG/TfgApplicationTests.java
+++ src/test/java/TFG/TfgApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.project;
+package TFG;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProjectApplicationTests {
+class TfgApplicationTests {
 
 	@Test
 	void contextLoads() {
